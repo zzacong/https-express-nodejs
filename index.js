7fc91922b0607041bb560c33d6e60744af75477b
Zhi Zao Ong <Zhi.Zao.Ong@ibm.com>
2021-03-04 23:03:56 +1100

    feat: https

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
-// const fs = require('fs')
-// const path = require('path')
-// const https = require('https')
+const fs = require('fs')
+const path = require('path')
+const https = require('https')
 
-// const certOptions = {
-// key: fs.readFileSync(path.resolve('cert/localhost.key')),
-// cert: fs.readFileSync(path.resolve('cert/localhost.crt')),
-// }
+const certOptions = {
+  key: fs.readFileSync(path.resolve('cert/localhost.key')),
+  cert: fs.readFileSync(path.resolve('cert/localhost.crt')),
+}
 
 const app = express()
 
@@ -16,21 +16,8 @@ app.get('/', (_, res) => {
   res.send({ msg: 'hello' })
 })
 
-app.post('/box', (req, res) => {
-  console.log('--- HEADERS ---')
-  console.log(req.headers)
-  console.log('--- BODY ---')
-  console.log(req.body)
-  res.send({ msg: 'Webhook notifications received' })
-})
+const server = https.createServer(certOptions, app)
 
-// const server = https.createServer(certOptions, app)
-
-// server.listen(443, () =>
-//   console.log(`Server is listening on https://localhost:443`)
-// )
-
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () =>
-  console.log(`Server is listening at http://localhost:${PORT}`)
+server.listen(443, () =>
+  console.log(`Server is listening on https://localhost:443`)
 )
